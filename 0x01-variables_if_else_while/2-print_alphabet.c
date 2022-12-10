4dccb1c8d41bc92c6aa0658982d928f79fac22f2
functional <omolarahabibah@gmail.com>
2022-12-10 21:52:09 +0100

    it's an alhabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets from 
+ * a - z
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 97;
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
