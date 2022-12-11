f2730472e1dd3d634be34a961825124727124414
functional <omolarahabibah@gmail.com>
2022-12-11 16:16:06 +0100

    myyyy

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<string.h>
+#include<unistd.h>
+
+/**
+* main - Prints out the last part of a quote in the standard error.
+* Return: 1 if success.
+*/
+int main(void)
+{
+	char buf1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, buf1, strlen(buf1));
+	return (1);
+}
