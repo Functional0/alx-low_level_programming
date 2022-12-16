3d3d6f4743bb9667036250bd82ada3e3d764f275
functional <omolarahabibah@gmail.com>
2022-12-16 07:47:26 +0100

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints 24 hour time
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 3; a++)
+	{
+		b = 0;
+		while (b < 4)
+		{
+			c = 0;
+			while (c < 6)
+			{
+				d = 0;
+				while (d < 10)
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+					d++;
+				}
+			c++;
+			}
+		b++;
+		}
+	}
+}
