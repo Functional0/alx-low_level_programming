438e54aa1551ce52df1ac4b8cc636f790810ddd6
functional <omolarahabibah@gmail.com>
2022-12-21 12:04:10 +0100

    6789

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * times_table - This prints times table from
+ * 0 to 9
+ *
+ * Return: 9
+ */
+void times_table(void)
+{
+	int a, b, ret;
+
+	ret = a * b;
+	for (a = 0; a 
