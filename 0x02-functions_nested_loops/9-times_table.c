5f042b8e25157226d12940ae7a787fab56615e94
functional <omolarahabibah@gmail.com>
2022-12-21 13:05:30 +0100

    x9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,5 +10,25 @@ void times_table(void)
 {
 	int a, b, ret;
 
-	ret = a * b;
-	for (a = 0; a 
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			ret = a * b;
+			if (ret > 9)
+			{
+				_putchar(ret / 10 + '0');
+				_putchar(ret % 10 + '0');
+			}
+			else
+				_putchar(ret + '0');
+			if (b != 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
